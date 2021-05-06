f5bd534e5d5950130551fb9345114283e7047885
Manuel María Guerrero Serrano <manuguese@gmail.com>
2021-05-06 10:48:43 +0200

    Refactor of return

--- babel_categories.py
+++ babel_categories.py
@@ -10,22 +10,37 @@ class BabelCategories(SemanticSource):
         self.complete_url = api['information_given_synset_url'] + key_part + incomplete_synset_part
 
     def find_metaphors(self, words: [(str, str)]) -> str:
-        #list of jsons containing categories
-        info_of_synsets_response = [requests.get(self.complete_url+w[1]).json() for w in words]
-        #list of list of json categories
-        categories_of_words = [w['categories'] for w in info_of_synsets_response]
-        #list of list of categories
-        category_names_of_words = [list(map(lambda elem : elem['category'], c)) for c in categories_of_words]
-        #TODO refactor
-        cat1 = sorted(category_names_of_words[0])
-        cat2 = sorted(category_names_of_words[2])
-        print(cat1)
-        print(cat2)
-        ret = 'hay metáfora'
-        for c in cat1:
-            if c in cat2:
-                ret = 'no hay metáfora'
-                print(c)
+        suj_word, suj_id = self.subject(words)
+        atr_word, atr_id = self.attribute(words)
+
+        info_of_suj = requests.get(self.complete_url+suj_id).json()
+        info_of_atr = requests.get(self.complete_url+atr_id).json()
+        
+        categories_of_suj = info_of_suj['categories']
+        categories_of_atr = info_of_atr['categories']
+        
+        category_names_of_suj = set(map(lambda elem : elem['category'], categories_of_suj))
+        category_names_of_atr = set(map(lambda elem : elem['category'], categories_of_atr))
+        
+        print(category_names_of_suj)
+        print(category_names_of_atr)
+        ret = {
+            'isMetaphor': True,
+            'relation': [],
+        }
+        for c in category_names_of_suj:
+            if c in category_names_of_atr:
+                ret['isMetaphor'] = False
+                ret['relation'].append(c)
+        
+        if ret['isMetaphor']:
+            ret['reason'] = suj_word + ' y ' + atr_word + ' no tienen ninguna categoría en común: ' + \
+                str(category_names_of_suj) + ' y ' + str(category_names_of_atr)
+        else:
+            ret['reason'] = suj_word + ' y ' + atr_word + 'comparten las categorías de:'
+            for c in ret['relation']:
+                ret['reason'] += ' ' + c + ','
+            ret['reason'] = ret['reason'][:-1]
         return ret
 
     def toString(self) -> str:
